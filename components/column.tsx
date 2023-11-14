5a449bd65ef7df4888cb73f72e2e0e9b077b1702
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-14 12:46:43 -0500

    Adds basic zustand configuration

--- components/column.tsx
+++ components/column.tsx
@@ -1,28 +1,27 @@
-import { Task } from "./task";
-
-const tasks = [
-  {
-    id: "1234",
-    title: "Our first task",
-    description: "Some description",
-    status: "DONE",
-  },
-];
+import { useMemo } from 'react';
+import { useTaskStore } from '@/lib/store';
+import { Task } from './task';
 
 export const Column = ({
   title,
-  status,
+  status
 }: {
   title: string;
   status: string;
 }) => {
-  const filteredTasks = tasks.filter((task) => task.status === status);
+  const tasks = useTaskStore(state => state.tasks);
+
+  // useMemo means that we only recalculate the filtered tasks when the tasks or status changes
+  const filteredTasks = useMemo(
+    () => tasks.filter(task => task.status === status),
+    [tasks, status]
+  );
 
   return (
-    <section className="h-[600px] flex-1">
-      <h2 className="text-white font-bold ml-1 font-2xl">{title}</h2>
-      <div className="h-full w-full rounded-xl bg-white/50 p-4 mt-3.5">
-        {filteredTasks.map((task) => (
+    <section className='h-[600px] flex-1'>
+      <h2 className='font-2xl ml-1 font-bold text-white'>{title}</h2>
+      <div className='mt-3.5 h-full w-full rounded-xl bg-white/50 p-4'>
+        {filteredTasks.map(task => (
           <Task key={task.id} {...task} />
         ))}
       </div>
