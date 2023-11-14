ab211275460e24fc750bbeced12ff2da08ffa255
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-14 12:12:48 -0500

    further project set up

--- components/column.tsx
+++ components/column.tsx
@@ -1,10 +1,31 @@
 import { Task } from "./task";
 
-export const Column = ({ title }: { title: string }) => {
+const tasks = [
+  {
+    id: "1234",
+    title: "Our first task",
+    description: "Some description",
+    status: "DONE",
+  },
+];
+
+export const Column = ({
+  title,
+  status,
+}: {
+  title: string;
+  status: string;
+}) => {
+  const filteredTasks = tasks.filter((task) => task.status === status);
+
   return (
     <section className="h-[600px] flex-1">
       <h2 className="text-white font-bold ml-1 font-2xl">{title}</h2>
-      <div className="h-full w-full rounded-xl bg-white/50 p-4 mt-3.5"></div>
+      <div className="h-full w-full rounded-xl bg-white/50 p-4 mt-3.5">
+        {filteredTasks.map((task) => (
+          <Task key={task.id} {...task} />
+        ))}
+      </div>
     </section>
   );
 };
--- components/columns.tsx
+++ components/columns.tsx
@@ -3,9 +3,9 @@ import { Column } from "./column";
 export const Columns = () => {
   return (
     <section className="flex mt-10 gap-6 lg:gap-12">
-      <Column title="Todo" />
-      <Column title="In progress" />
-      <Column title="Done" />
+      <Column title="Todo" status="TODO" />
+      <Column title="In progress" status="IN_PROGRESS" />
+      <Column title="Done" status="DONE" />
     </section>
   );
 };
